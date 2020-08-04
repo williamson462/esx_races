2986a4278c8a59765d0f8b3fac980fab5791df53
MeaslystormNetwork <45130331+williamson462@users.noreply.github.com>
2020-08-04 13:03:52 +0100

    Uploaded Config.en
    I have translated it to english config file :)

---
+++ locales/en.lua
@@ -0,0 +1,104 @@
+Locales['en'] = {
+  -- ********** SOLO **********
+  -- Blips
+  ['solo_key']            = 'Against the clock',
+  ['solo_listing']        = 'List of circuits',
+  -- collect
+  ['press_collect_solo']  = 'Press to ~INPUT_CONTEXT~ to retrieve \ ra key ~y~against the watch~s~',
+  ['inv_full_solo_key']   = 'You can no longer pick up a key \n~r~against the watch~s~',
+  -- register
+  ['press_solo_race_list']= 'Press to ~INPUT_CONTEXT~ to access ~y~circuits~s~',
+  ['register_ok']         = 'Inscription ~y~carried out~s~\rGo to the point of ~r~departure~s~',
+  ['already_register']    = 'Inscription ~r~already~s~ carried out\Go to the point of ~r~departure~s~',
+  ['already_multi']       = 'A street race is ~r~already~s~ In progress',
+  ['no_solo_key']         = 'You dont have a key ~r~against the watch~s~',
+  -- race
+  ['ready_to_start']      = 'Warning to ~r~departure~s~',
+  ['race_chrono']         = '~b~%s~s~\rTimes: ~y~%s~s~\rCheckpoint: ~y~%s/%s~s~',
+  ['race_in_vehicle']     = 'Get into a ~r~VEHICLES~s~ !!\r~r~%s~s~',
+  ['race_loose']          = 'You have ~r~LOST~s~ !!!',
+  ['nice_ride']           = 'Well done race over ~y~%s~s~',
+  ['new_record']          = 'New record ~r~%s~s~',
+	-- Menu
+  ['own_stat']            = 'Stats personnel',
+  ['daily_stat']          = 'Daily Stats',
+  ['monthly_stat']        = 'Monthly Stats',
+  ['registration']        = 'Registration',
+  ['own_title']           = '%s - Lost',
+  ['daily_title']         = '%s - Daily',
+  ['monthly_title']       = '%s - Monthly',
+  ['record_notif']        = 'Time: ~b~%s~s~\nParticipant: ~r~%s~s~\nle ~y~%s~s~ à ~y~%s~s~',
+  -- ********** MULTI **********
+  -- Blips
+  ['multi_key']             = 'Street racing key',
+  ['multi_listing']         = 'Shopping list',
+  -- collect
+  ['press_collect_multi']   = 'Press to ~INPUT_CONTEXT~ to recover\key rune ~y~street race~s~',
+  ['inv_full_multi_key']    = 'You can no longer pick up a key\n~r~street race~s~',
+  -- ranking
+  ['multi_record_notif']    = '~b~%s~s~ | ~b~%s~s~\nParticipant: ~r~%s~s~\nle ~y~%s~s~ à ~y~%s~s~',
+  -- register
+  ['press_multi_race_list'] = 'Press to ~INPUT_CONTEXT~ to access ~y~courses~s~',
+  ['no_race_ended']         = 'No completed races',
+  ['no_multi_key']          = 'You dont have a key ~r~street race~s~',
+  ['multi_register_full']   = 'Impossible number of participants ~r~MAX~s~',
+  ['multi_register_ok']     = 'Inscription ~y~carried out~s~\Wait the ~r~departure~s~',
+  -- edit
+  ['race_already_exist']    = 'A street race has ~r~already~s~ was created',
+  ['create_in_prog']        = '~y~Creation~s~ carried out',
+  ['multi_change_fail']     = 'Invalid value',
+  -- race
+  ['multi_ready_to_start']  = 'Prepare for ~r~start~s~',
+  ['multi_wait_to_start']   = 'Wait for all the participants.',
+  ['multi_race_start']      = 'Warning to ~r~departure~s~',
+  ['multi_race_chrono']     = '~b~%s~s~\rTemps: ~y~%s~s~\t\rTour: ~y~%s/%s~s~\rPos: ~y~%s/%s~s~',
+  -- Menu home
+  ['multi_home_title']      = 'Street race',
+  ['ended_races']           = 'Classements',
+  ['show_registration']     = 'See my listing',
+  ['create_race']           = 'Create a street race',
+  ['edit_race']             = 'Edit street racing',
+  -- Menu ranking
+  ['multi_rank_title']      = 'Classements',
+  ['multi_rank_own']        = 'Classement Perso',
+  ['multi_rank_race']       = '%s Tours - %s Pers - %s',
+  ['multi_rank_own_race']   = '%s - %stours %spers - %s - %s',
+  ['multi_rank_multi_title']= '%s - %s tours, %s pers',
+  ['multi_rank_multi_race'] = '%s - %s - %s',
+  -- Menu register
+  ['multi_register_title']     = 'Select a race',
+  ['multi_register_list']      = '%s: %s tours, %s pers',
+  ['multi_my_register_title']  = 'My registration',
+  ['multi_register_registerC'] = 'Registration closed',
+  ['multi_register_registerO'] = 'Open registration',
+  ['multi_register_readyC']    = 'Waiting for departure',
+  ['multi_register_readyO']    = 'Ready to go',
+  -- Menu edit
+  ['multi_edit_title']      = 'Name - %s',
+  ['multi_edit_race']       = 'Race: %s',
+  ['multi_edit_laps']       = 'Number Of Laps: %s',
+  ['multi_edit_pers']       = 'Number of participants: %s',
+  ['multi_edit_registerC']  = 'Close registrations',
+  ['multi_edit_registerO']  = 'Open registrations',
+  ['multi_edit_readyC']     = 'Stop the race',
+  ['multi_edit_readyO']     = 'Start the race',
+  ['remove_multi']          = 'Cancel the race',
+  -- Menu input
+  ['multi_change_laps_title'] = 'Number Of Laps',
+  ['multi_change_pers_title'] = 'Number Of Players',
+  ['multi_register_list']     = '%s: %s tours, %s pers',
+  -- ********** COMMON **********
+  ['pickup_in_prog']      = '~y~Ramassage~s~ en cours',
+  ['pickup_retry']        = '~r~Attendre~s~ et Réessayez',
+  ['out_vehicle']         = 'Vous devez sortir du ~r~véhicule~s~',
+  ['act_imp_police']      = 'Action ~r~impossible~s~,\r~b~policiers~s~: ~r~%s/%s~s~',
+  ['pickup_ok']           = 'Ramassage ~y~effectuée~s~',
+  ['in_vehicle']          = 'Vous devez être dans un ~r~véhicule~s~',
+  ['exit_marker']         = 'Press to ~INPUT_CONTEXT~ pour annuler le ~y~process~s~',
+  ['no_race']             = 'No ~r~circuits~s~',
+  ['press_start_race']    = 'Press to ~INPUT_CONTEXT~ pour commencer la ~y~course~y~',
+  ['no_record']           = 'No ~r~temps~s~',
+  ['remove_register']     = 'Cancel registration',
+  ['removed_register']    = 'Inscription ~r~canceled~s~',
+  ['value_between']       = 'La valeur doit être comprise\rentre ~r~%s et %s~s~',
+}
